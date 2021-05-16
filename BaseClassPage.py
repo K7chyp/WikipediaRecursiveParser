dd0b9967ede8e4a0a24718f9616f92e0c5f2bf52
Nikita <nekitos199@gmail.com>
2021-05-16 10:51:21 +0300

    Add giving text from page

--- BaseClassPage.py
+++ BaseClassPage.py
@@ -1,8 +1,15 @@
 from BaseClassBrowser import BaseClassPageSettings
 from SubFunctions import get_all_product_hrefs_from_page
+from re import sub
 
-class WikipediaPageParser(BaseClassPageSettings): 
-    def __init__(self, url): 
-        self.url = url
+
+class WikipediaPageParser(BaseClassPageSettings):
+    def __init__(self, url) -> None:
+        self.url: str = url
         super().__init__(self.url)
-        self.hrefs = get_all_product_hrefs_from_page(self.soup)
+        self.hrefs: list = get_all_product_hrefs_from_page(self.soup)
+        self.text_from_page: str = sub(
+            "\[[0-9]+\]",
+            "",
+            "".join([part_of_page.text for part_of_page in self.soup.find_all("p")]),
+        )
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from BaseClassPage import WikipediaPageParser
 
 
 def main():
-    print(WikipediaPageParser("https://en.wikipedia.org/wiki/Python").hrefs)
+    print(
+        WikipediaPageParser(
+            "https://en.wikipedia.org/wiki/Python_(programming_language)"
+        ).text_from_page
+    )
 
 
 if __name__ == "__main__":
