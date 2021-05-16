dd6867dca35a1165f4d31e758db8a3622d8a842a
Nikita <nekitos199@gmail.com>
2021-05-16 10:39:10 +0300

    Some fixes

--- BaseClassPage.py
+++ BaseClassPage.py
@@ -1,7 +1,7 @@
 from BaseClassBrowser import BaseClassPageSettings
 from SubFunctions import get_all_product_hrefs_from_page
 
-class WikipediaMainPageParser(BaseClassPageSettings): 
+class WikipediaPageParser(BaseClassPageSettings): 
     def __init__(self, url): 
         self.url = url
         super().__init__(self.url)
