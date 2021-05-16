87f476b7d1bb875c5ff5069c894b42ac1ed68949
Nikita <nekitos199@gmail.com>
2021-05-16 10:53:12 +0300

    Addd simplicity

--- BaseClassPage.py
+++ BaseClassPage.py
@@ -7,6 +7,9 @@ class WikipediaPageParser(BaseClassPageSettings):
     def __init__(self, url) -> None:
         self.url: str = url
         super().__init__(self.url)
+        self.get_coomon_info_about_page()
+
+    def get_coomon_info_about_page(self):
         self.hrefs: list = get_all_product_hrefs_from_page(self.soup)
         self.text_from_page: str = sub(
             "\[[0-9]+\]",
